0b75785588dd79ec8cbaf8dd40ce6e2cf4112ab8
Eliseo-Laborde <Elaborde1@mail.austral.edu.ar>
2025-04-14 15:04:35 -0300

    Update leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -1,2 +1,8 @@
 def leap_year():
-    print("TO DO")
+    anio=int(input("Ingrese un año: "))
+    if anio%4!=0:
+        print(f"El año {anio} no es bisiesto")
+    elif anio%400!=0 and anio%100==0:
+        print(f"El año {anio} no es bisiesto")
+    elif anio%4==0:
+        print(f"El año {anio} es bisiesto")
